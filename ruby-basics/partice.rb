25d50010f242a1956dba9bd96eab89e9aa3c3977
shahriar-shakil <toshahriarshakil@gmail.com>
2024-09-06 02:11:44 +0600

    basic ruby

---
+++ ruby-basics/partice.rb
@@ -0,0 +1,26 @@
+#variables 
+
+var = 10 
+puts var.class
+var = "ruby" 
+puts var.class
+
+# Definition
+# A variable is a named storage location in memory that holds a value. The value stored can be changed during the execution of a program.
+# variables types
+# global variables => $var
+# class variables => @@var
+# instance variables => @var
+# local variables => var
+
+#Constants 
+# A constant is a named value that cannot be changed once it has been assigned.
+# Constants are typically denoted by uppercase letters and underscores.
+# constant definition
+# CONSTANT_NAME = value
+
+class Example 
+end
+
+VERSION = 10.02 # constant  . cant change
+puts VERSION.class
