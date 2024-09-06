11b827ecd8cba2b234e5dc5cb88db6010cbbbe50
shahriar-shakil <toshahriarshakil@gmail.com>
2024-09-06 19:55:11 +0600

    if and unless condition

---
+++ ruby-basics/condition.rb
@@ -0,0 +1,18 @@
+#if statement 
+
+puts "please enter a number"
+
+num = gets.to_i
+# if num == 0
+#   puts "Given number is 0"
+# elsif(num%2 == 0)
+#   puts "#{num} is an even number"
+# else
+#   puts "#{num} is an odd number"
+# end
+
+unless num % 2 == 0 
+  puts "#{num} is an odd number"
+else 
+  puts "#{num} is an even number"
+end
